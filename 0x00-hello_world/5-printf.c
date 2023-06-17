b6dd8f43e1bb38f258f0ae66f33294364d80be2e
Alfred <alfredaudu57@gmail.com>
2023-06-17 08:01:47 -0700

    m

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
- *main - Entry point
- *
- * Return: Always 0 (Success)
+
+/**
+ *main - This prints a sentence *
+ * Return: returns 0
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
