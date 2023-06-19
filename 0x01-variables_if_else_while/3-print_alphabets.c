7bd4f45cac776289f1fba526f74ea8b887eb9ffa
Alfred <alfredaudu57@gmail.com>
2023-06-19 13:17:49 -0700

    f

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Using putchar
+ *
+ * Return: still 0
+ */
+
+int main(void)
+{
+	char c = 'a';
+
+	char d = 'A';
+
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+	
+	putchar('\n');
+	return (0);
+}
