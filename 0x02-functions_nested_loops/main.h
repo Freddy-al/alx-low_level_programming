674d1b9c9ba555494d9618131dff3b29a3b800fb
Alfred <alfredaudu57@gmail.com>
2023-06-24 02:24:48 -0700

    fix

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,9 @@
-#ifndef main.h
-#define main.h
+#ifndef MAIN_H
+#define MAIN_H
 
-	int _putchar(char c)
+#include <stdio.h>
+
+	int _putchar(char c);
 	void print_alphabet(void);
 	void print_alphabet_x10(void);
 	int _islower(int c);
@@ -15,5 +17,5 @@
 	void print_to_98(int n);
 	void print_times_table(int n);
 
-#endif  // MAIN_H
+#endif
 
